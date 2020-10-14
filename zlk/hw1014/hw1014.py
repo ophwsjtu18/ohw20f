9f9ade480da5164b8700cdf6b05551c066f2b9b7
lintonfirst <sczlk@163.com>
2020-10-14 21:03:09 +0800

    hw: 1014

---
+++ zlk/hw1014/hw1014.py
@@ -0,0 +1,7 @@
+import numpy as np
+import cv2
+# Load an color image in grayscale
+img = cv2.imread('Sketchpad.png',cv2.IMREAD_COLOR)
+cv2.imshow('image',img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
