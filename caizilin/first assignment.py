47b0a3a929d4cb4010329639e04592ceeab4262e
PatronusCai <72862651+PatronusCai@users.noreply.github.com>
2020-10-17 21:42:39 +0800

    Add files via upload

---
+++ caizilin/first assignment.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 17 18:49:00 2020
+
+@author: CaiZilin
+"""
+import numpy as np
+import cv2
+img = cv2.imread('IMG_1240.JPG',cv2.IMREAD_COLOR)
+cv2.imshow('picture',img)
+cv2.waitKey(0)
