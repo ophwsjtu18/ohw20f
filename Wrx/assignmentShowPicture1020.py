31b256f6fb2966b4dfe19d5e28f4d9dab9e281f2
WangRuixiangSJTU <72863034+WangRuixiangSJTU@users.noreply.github.com>
2020-10-20 23:43:43 +0800

    Update assignmentShowPicture1020.py

--- Wrx/assignmentShowPicture1020.py
+++ Wrx/assignmentShowPicture1020.py
@@ -1,5 +1,5 @@
 import numpy as np
 import cv2
-img=cv2.imread(r'C:\Users\wrx\Desktop\loveLetter.jpg',1)
+img=cv2.imread(r'loveLetter.jpg',1)
 cv2.namedWindow('image',cv2.WINDOW_NORMAL)
 cv2.imshow('image',img)
