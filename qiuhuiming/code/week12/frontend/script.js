d0ed6ca6127fa65c29ca36aec496c97647a9ba3f
qiuhuiming <1985849912@qq.com>
2020-11-25 22:22:27 +0800

    Add basic website

---
+++ qiuhuiming/code/week12/frontend/script.js
@@ -0,0 +1,8 @@
+// get the buttons 
+const upButton = document.querySelector('#up-button')
+const downButton = document.querySelector('#down-button')
+const leftButton = document.querySelector('#left-button')
+const rightButton = document.querySelector('#right-button')
+
+const arrowButtons = [upButton, downButton, leftButton, rightButton]
+
