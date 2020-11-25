401763076481ad0296d6cdc0794e84edd4286f8a
qiuhuiming <1985849912@qq.com>
2020-11-25 22:46:33 +0800

    Feat sent ajax request to the server

--- qiuhuiming/code/week12/frontend/script.js
+++ qiuhuiming/code/week12/frontend/script.js
@@ -3,6 +3,20 @@ const upButton = document.querySelector('#up-button')
 const downButton = document.querySelector('#down-button')
 const leftButton = document.querySelector('#left-button')
 const rightButton = document.querySelector('#right-button')
-
 const arrowButtons = [upButton, downButton, leftButton, rightButton]
 
+const clickCallbackFactory = (id) => {
+    return () => {
+        const arrow = id.substring(0, id.indexOf('-'))
+        console.log(arrow)
+        $.get('/cgi-bin/handle_arrow.py', 
+        {
+            arrow
+        })
+    }
+}
+
+arrowButtons.forEach((button) => {
+    button.addEventListener('click', clickCallbackFactory(button.id))
+}
+)
