d4998e2bbc02e2380ea3bfc90c3019098b58044d
wdk1319 <72863010+wdk1319@users.noreply.github.com>
2020-12-02 14:06:33 +0800

    Update homework1.py

--- wdk/assignment/7/homework1.py
+++ wdk/assignment/7/homework1.py
@@ -6,26 +6,27 @@ from mcpi.minecraft import Minecraft
 
 mc = Minecraft.create("127.0.0.1",4711)
 
-for i in range(10):
-    pos=mc.player.getTilePos()
-    print(pos)
-    mc.player.setTilePos(pos.x+1,pos.y,pos.z)
-    sleep(1)
-
-for i in range(10):
-    pos=mc.player.getTilePos()
-    print(pos)
-    mc.player.setTilePos(pos.x-1,pos.y,pos.z)
-    sleep(1)
-
-for i in range(10):
-    pos=mc.player.getTilePos()
-    print(pos)
-    mc.player.setTilePos(pos.x,pos.y,pos.z+1)
-    sleep(1)
-
-for i in range(10):
-    pos=mc.player.getTilePos()
-    print(pos)
-    mc.player.setTilePos(pos.x,pos.y,pos.z-1)
-    sleep(1)
+while True:
+    for i in range(10):
+        pos=mc.player.getTilePos()
+        print(pos)
+        mc.player.setTilePos(pos.x+1,pos.y,pos.z)
+        sleep(1)
+    
+    for i in range(10):
+        pos=mc.player.getTilePos()
+        print(pos)
+        mc.player.setTilePos(pos.x-1,pos.y,pos.z)
+        sleep(1)
+    
+    for i in range(10):
+        pos=mc.player.getTilePos()
+        print(pos)
+        mc.player.setTilePos(pos.x,pos.y,pos.z+1)
+        sleep(1)
+    
+    for i in range(10):
+        pos=mc.player.getTilePos()
+        print(pos)
+        mc.player.setTilePos(pos.x,pos.y,pos.z-1)
+        sleep(1)
