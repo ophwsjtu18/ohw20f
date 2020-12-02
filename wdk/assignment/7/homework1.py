1e5c284a04caab62e26e9ce54fcf461d88121793
wdk1319 <72863010+wdk1319@users.noreply.github.com>
2020-12-02 13:55:02 +0800

    Create homework1.py

---
+++ wdk/assignment/7/homework1.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+
+import os
+from time import sleep
+from mcpi.minecraft import Minecraft
+
+mc = Minecraft.create("127.0.0.1",4711)
+
+for i in range(10):
+    pos=mc.player.getTilePos()
+    print(pos)
+    mc.player.setTilePos(pos.x+1,pos.y,pos.z)
+    sleep(1)
+
+for i in range(10):
+    pos=mc.player.getTilePos()
+    print(pos)
+    mc.player.setTilePos(pos.x-1,pos.y,pos.z)
+    sleep(1)
+
+for i in range(10):
+    pos=mc.player.getTilePos()
+    print(pos)
+    mc.player.setTilePos(pos.x,pos.y,pos.z+1)
+    sleep(1)
+
+for i in range(10):
+    pos=mc.player.getTilePos()
+    print(pos)
+    mc.player.setTilePos(pos.x,pos.y,pos.z-1)
+    sleep(1)
