db36279ab1869ac5b80fef073c3794d4537f8b0c
dmlyq <72863951+dmlyq@users.noreply.github.com>
2020-12-09 19:00:09 +0800

    Create voice.py

---
+++ dml/voice.py
@@ -0,0 +1,164 @@
+from moocxing.package import MOOCXING
+from moocxing.robot.Brain import Brain
+
+mx = MOOCXING()
+
+# 自己注册百度API
+APP_ID = '23084377'
+API_KEY = 'L2OAvTghiV1VOdno0FiParBM'
+SECRET_KEY = 'ldStCugATRfh3ibzOyMAhFVpGEw1efXu'
+
+# 初始化播放器和录音模块
+media = mx.initMedia()
+# 初始化多线程模块
+thread = mx.initThread()
+# 初始化MQTT模块
+mqtt = mx.initMqtt()
+# 初始化拼音模块
+pinyin = mx.initPinyin()
+# 初始化我的世界
+mc = mx.initMinecraft()
+# 初始化串口模块
+serial = mx.initSerial(com="COM31",bps=9600)
+# 初始化语音识别+语音合成模块
+speech = mx.initSpeech(APP_ID, API_KEY, SECRET_KEY)
+# 初始化NLP模块
+nlp = mx.initNLP(APP_ID, API_KEY, SECRET_KEY)
+
+pos=mc.player.getTilePos()
+distance=0
+
+print("******************初始化完成******************\n")
+
+# 初始化技能插件
+brain = Brain({"media": media,
+               "speech": speech,
+               "nlp": nlp,
+               "mqtt": mqtt,
+               "serial": serial,
+               "thread": thread,
+               "pinyin": pinyin,
+               "minecraft": mc})
+
+print("***************技能插件加载完成***************\n")
+
+# 语音合成+播放
+def TTSPlay(text):
+    speech.TTS(text)
+    media.play()
+
+# 录音+语音识别
+def recordSTT():
+    media.record()
+    return speech.STT()
+
+
+while True:
+    result = recordSTT()
+    pos=mc.player.getTilePos()
+    
+
+
+
+    '''
+    brain.query(result,_print = False)
+    参数: 
+        result: 语言识别内容
+        _print: 是否打印识别内容
+    返回: 技能是否匹配成功(True/False)
+
+    技能: 查时间、查天气、听音乐
+    查时间关键词: 时间、几点、日期、日子、几号、星期
+    查天气关键词: 天气（要加城市）
+    听音乐关键词: 听、播放、首、歌 + 歌名
+                暂停、继续、停止
+                (网易云API部分歌曲可能不版权)
+    '''
+    if not brain.query(result, _print=True):
+        distance=0
+        if "左转" in result:
+            TTSPlay("好的，向左转")
+            if "30度" in result:
+                mc.player.setRotation(330)
+                serial.send("0")
+            if "45度" in result:
+                mc.player.setRotation(315)
+                serial.send("1")
+            if "60度" in result:
+                mc.player.setRotation(300)
+                serial.send("2")
+            if "90度" in result:
+                mc.player.setRotation(270)
+                serial.send("3")
+            if "120度" in result:
+                mc.player.setRotation(240)
+                serial.send("4")
+            if "150度" in result:
+                mc.player.setRotation(210)
+                serial.send("5")
+            if "180度" in result:
+                mc.player.setRotation(180)
+                serial.send("7")
+    
+        if "右转" in result:
+            TTSPlay("好的，向右转")
+            if "30度" in result:
+                mc.player.setRotation(30)
+                serial.send("0")
+            if "45度" in result:
+                mc.player.setRotation(45)
+                serial.send("1")
+            if "60度" in result:
+                mc.player.setRotation(60)
+                serial.send("2")
+            if "90度" in result:
+                mc.player.setRotation(90)
+                serial.send("3")
+            if "120度" in result:
+                mc.player.setRotation(120)
+                serial.send("4")
+            if "150度" in result:
+                mc.player.setRotation(150)
+                serial.send("5")
+            if "180度" in result:
+                mc.player.setRotation(180)
+                serial.send("7")
+
+        if"1步" or "一步"  in result:
+            distance=1
+        if"2步" or "两步" in result:
+            distance=2
+        if"3步" or "三步" in result:
+            distance=3
+        if"4步" or "四步" in result:
+            distance=4
+        if"5步" or "五步" in result:
+            distance=5
+        if"6步" or "六步" in result:
+            distance=6
+        if"7步" or "七步" in result:
+            distance=7
+        if"8步" or "八步" in result:
+            distance=8
+            print(distance)
+        if"9步" or "九步" in result:
+            distance=9
+        if"10步" or "十步" in result:
+            distance=10
+        print(distance)
+        if"向前走"in result:
+            TTSPlay("好的，向前走")
+            print(distance)
+            mc.player.setTilePos(pos.x+distance,pos.y,pos.z)
+        if"向后走"in result:
+            TTSPlay("好的，向后走")
+            mc.player.setTilePos(pos.x-distance,pos.y,pos.z)
+        if"向左走"in result:
+            TTSPlay("好的，向左走")
+            mc.player.setTilePos(pos.x,pos.y,pos.z-distance)
+        if"向右走"in result:
+            TTSPlay("好的，向右走")
+            mc.player.setTilePos(pos.x,pos.y,pos.z+distance)
+
+            
+            
